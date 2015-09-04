05187a44c60506ab66694aa37f92df735b261c2f
Blaine Schmeisser <blainesch@gmail.com>
2015-09-03 20:45:54 -0700

    Get input!

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <mruby.h>
 #include <mruby/compile.h>
@@ -9,8 +10,15 @@
 
 static mrb_value my_c_method(mrb_state *mrb, mrb_value value)
 {
-  puts("C Method");
+  puts("CD Method");
   return value;
+}
+
+static mrb_value getstring(mrb_state *mrb, mrb_value value)
+{
+  char response[100];
+  fgets(response, 100, stdin);
+  return mrb_str_new_cstr(mrb, response);
 }
 
 int main(void)
@@ -22,6 +30,7 @@ int main(void)
   // MyModule::my_c_method()
   struct RClass *mymodule = mrb_define_module(mrb, "MyModule");
   mrb_define_class_method(mrb, mymodule, "my_c_method", my_c_method, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, mymodule, "getstring", getstring, MRB_ARGS_NONE());
 
   // Run sqlite3 gem init so we can use it in mruby_code.rb
   mrb_mruby_sqlite3_gem_init(mrb);
